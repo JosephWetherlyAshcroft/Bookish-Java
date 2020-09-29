ed4278219bdc43fde761e6d379913933454fead4
Emily Rudling <mmrudling@yahoo.com>
2020-09-29 11:51:36 +0100

    Accessed the database, output book "list", created book class

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -1,17 +1,22 @@
 package org.softwire.training.bookish;
+import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.reflect.ConstructorMapper;
+import org.softwire.training.bookish.models.database.Book;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         String hostname = "127.0.0.1";
-        String database = "Library";
+        String database = "mydb";
         String user = "root";
-        String password = "bookish";
+        String password = "BookishPassword";
         String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false&allowPublicKeyRetrieval=true";
 
         jdbcMethod(connectionString);
@@ -25,9 +30,6 @@ public class Main {
         // See this page for details: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
 
         Connection connection = DriverManager.getConnection(connectionString);
-
-
-
     }
 
     private static void jdbiMethod(String connectionString) {
@@ -36,10 +38,22 @@ public class Main {
         // TODO: print out the details of all the books (using JDBI)
         // See this page for details: http://jdbi.org
         // Use the "Book" class that we've created for you (in the models.database folder)
-
         Jdbi jdbi = Jdbi.create(connectionString);
+        Handle handle = jdbi.open();
+        handle.registerRowMapper(ConstructorMapper.factory(Book.class));
+//        List<Book> books = handle.createQuery("SELECT * FROM Books ORDER BY ID")
+//                    .mapTo(Book.class)
+//                    .list();
+        List<Book> books = handle.createQuery("SELECT id, ISBN, noOfCopies, name, availableCopies FROM user")
+                .map((rs, ctx) -> new Book(rs.getInt("ID"), rs.getLong("ISBN"), rs.getInt("noOfCopies"), rs.getString("name"), rs.getInt("availableCopies")))
+                .list();
 
 
 
+        for(int i = 0; i < books.size(); i++){
+            Book book = books.get(i);
+            System.out.println("ID, Title, ISBN, Number of copies, Available copies");
+            System.out.println(book.getID() + ", " + book.getTitle() + ", " + book.getISBN()  + ", " + book.getNoOfCopies()  + ", " + book.getAvailableCopies());
+        }
     }
 }
--- src/main/java/org/softwire/training/bookish/models/database/Book.java
+++ src/main/java/org/softwire/training/bookish/models/database/Book.java
@@ -1,5 +1,35 @@
 package org.softwire.training.bookish.models.database;
 
-public class Book {
+import java.math.BigInteger;
 
+public class Book {
+    int ID;
+    long ISBN;
+    int noOfCopies;
+    String title;
+    int availableCopies;
+
+    public Book(int id, long isbn, int noofcopies, String name, int availablecopies){
+        ID = id;
+        ISBN = isbn;
+        noOfCopies = noofcopies;
+        title = name;
+        availableCopies = availablecopies;
+    }
+
+    public long getISBN() {
+        return ISBN;
+    }
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+    public int getID() {
+        return ID;
+    }
+    public int getNoOfCopies() {
+        return noOfCopies;
+    }
+    public String getTitle() {
+        return title;
+    }
 }
